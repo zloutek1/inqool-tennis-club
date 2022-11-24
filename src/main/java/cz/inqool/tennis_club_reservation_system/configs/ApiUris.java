607be60b8765a533c520033507b1e231999a3896
zloutek1 <zloutek1@gmail.com>
2022-11-24 14:22:26 +0100

    feat: added model controller api paths

--- src/main/java/cz/inqool/tennis_club_reservation_system/configs/ApiUris.java
+++ src/main/java/cz/inqool/tennis_club_reservation_system/configs/ApiUris.java
@@ -12,6 +12,7 @@ public abstract class ApiUris {
     public static final String USER_DELETE = "/user/delete/{id}";
     public static final String USERS = "/user/";
     public static final String USER = "/user/{id_or_username}";
+    public static final String USER_RESERVATIONS = "/user/{id_or_username}/reservations";
     public static final String USER_ROLE_ADD = "/user/{username}/role/{roleName}/add";
     public static final String USER_ROLE_REMOVE = "/user/{username}/role/{roleName}/remove";
 
@@ -19,5 +20,16 @@ public abstract class ApiUris {
     public static final String ROLE_EDIT = "/role/edit";
     public static final String ROLE_DELETE = "/role/delete/{id}";
     public static final String ROLES = "/role/";
+
+    public static final String COURT_NEW = "/court/new";
+    public static final String COURT_EDIT = "/court/edit";
+    public static final String COURT_DELETE = "/court/delete/{id}";
+    public static final String COURTS = "/court/";
+    public static final String COURT_RESERVATIONS = "/court/{id}/reservations";
+
+    public static final String RESERVATION_NEW = "/reservation/new";
+    public static final String RESERVATION_EDIT = "/reservation/edit";
+    public static final String RESERVATION_DELETE = "/reservation/delete/{id}";
+    public static final String RESERVATIONS = "/reservation/";
 
 }
